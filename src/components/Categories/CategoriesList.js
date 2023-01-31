b2922f0b1b58ec281a182e54d18078e376521b28
nathanpyles.91@gmail.com <nathanpyles.91@gmail.com>
2023-01-31 16:24:02 -0500

    Edit-Delete buttons

--- src/components/Categories/CategoriesList.js
+++ src/components/Categories/CategoriesList.js
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import "./Categories.css"
 
-    // const localRainbowUser = localStorage.getItem("rainbow_user")
-    // const rainbowUserObject = JSON.parse(localRainbowUser)
+// const localRainbowUser = localStorage.getItem("rainbow_user")
+// const rainbowUserObject = JSON.parse(localRainbowUser)
 
 export const CategoryList = () => {
-    const [categories, setCategories] = useState ([])
+    const [categories, setCategories] = useState([])
     // const [filteredPosts, setFiltered] = useState([])
     const navigate = useNavigate()
 
@@ -33,27 +33,39 @@ export const CategoryList = () => {
         fetch(`http://localhost:8088/categories`)
             .then((res) => res.json())
             .then((categoryArray) => {
-            setCategories(categoryArray)
+                setCategories(categoryArray)
             })
     }, [])
 
-        return (
-            <>
+    return (
+        <>
             <div className="top-of-posts">
                 <h1 className="posts-title">Categories</h1>
             </div>
-            
+
             <div className="categories-container">
                 {categories.map((categoryObj) => {
                     return (
-                        <div className="activity-card" key={categoryObj.id}>
-                        <div className="each-post">
-                            <h3 className="categories-title">{categoryObj.label}</h3>
-                        </div>
+                        <div className="category-card">
+                            <div className="category-button">
+                                <div>
+                                    <button class="button is-small is-dark">Edit</button>
+                                </div>
+                            </div>
+                            <div className="category-button">
+                                <div>
+                                    <button class="button is-small is-dark">Delete</button>
+                                </div>
+                            </div>
+                            <div className="activity-card" key={categoryObj.id}>
+                                <div className="each-post">
+                                    <h3 className="categories-title">{categoryObj.label}</h3>
+                                </div>
+                            </div>
                         </div>
                     )
                 })}
-                </div>
-            </>
-        )
-    }
+            </div>
+        </>
+    )
+}
