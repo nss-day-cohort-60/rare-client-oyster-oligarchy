24f10dec91acd6262035b5a3f3956d8f61f11a97
nathanpyles.91@gmail.com <nathanpyles.91@gmail.com>
2023-02-01 10:50:22 -0500

    Pulling new main

--- src/components/Categories/CategoriesList.js
+++ src/components/Categories/CategoriesList.js
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom"
 import { AddCategory } from "./AddCategoryForm"
 import "./Categories.css"
 
-    // const localRainbowUser = localStorage.getItem("rainbow_user")
-    // const rainbowUserObject = JSON.parse(localRainbowUser)
+// const localRainbowUser = localStorage.getItem("rainbow_user")
+// const rainbowUserObject = JSON.parse(localRainbowUser)
 
 export const CategoryList = () => {
-    const [categories, setCategories] = useState ([])
+    const [categories, setCategories] = useState([])
     // const [filteredPosts, setFiltered] = useState([])
     const [value, setValue] = useState('')
     const navigate = useNavigate()
@@ -35,26 +35,26 @@ export const CategoryList = () => {
         fetch(`http://localhost:8088/categories`)
             .then((res) => res.json())
             .then((categoryArray) => {
-            setCategories(categoryArray)
+                setCategories(categoryArray)
             })
     }, [])
     const rerender = () => {
         fetch(`http://localhost:8088/categories`)
             .then((res) => res.json())
             .then((categoryArray) => {
-            setCategories(categoryArray)
+                setCategories(categoryArray)
             })
     }
-   
+
     const handleChange = (event) => {
         setValue(event.target.value);
-        
-      };
-      const handleSaveButtonClick = (event) => {
+
+    };
+    const handleSaveButtonClick = (event) => {
         event.preventDefault()
         const categoryToSendToAPI = {
             label: value
-            
+
         }
         return fetch(`http://localhost:8088/categories`, {
             method: "POST",
@@ -63,39 +63,44 @@ export const CategoryList = () => {
             },
             body: JSON.stringify(categoryToSendToAPI)
         })
-        .then (rerender)
-        
-  
-        
-        
-      
+            .then(rerender)
+
+
+
+
+
     }
-    
-        return (
-            <>
+
+    return (
+        <>
             <div className="top-of-posts">
                 <h1 className="posts-title">Categories</h1>
             </div>
-            
+            <div className="categories">
             <div className="categories-container">
-                {categories.map((categoryObj) => {
-                    return (
-                        <div className="activity-card" key={categoryObj.id}>
-                        <div className="each-post">
-                            <h3 className="categories-title">{categoryObj.label}</h3>
-                        </div>
-                        </div>
-                        
-                    )
+                    {categories.map((categoryObj) => {
+                        return (
+                            <div className="activity-card" key={categoryObj.id}>
+                                <div className="each-post">
+                                    <h3 className="categories-title">{categoryObj.label}</h3>
+                                </div>
+                            </div>
+            
+                )
                 })}
-                <form>
-        <label htmlFor="categoryadd">Add New Category</label>
-      <input className="categoryadd" type="text" value={value} onChange={handleChange} />
-      <button
-      onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
-      >Save Category</button>
-    </form>
-                </div>
-            </>
-        )
-    }
+            </div>
+                <aside>
+                <form className="category-search">
+                    <label htmlFor="categoryadd">Add New Category</label>
+                    <input className="categoryadd" type="text" value={value} onChange={handleChange} />
+                    <button
+                        onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
+                    >Save Category</button>
+                </form>
+                </aside>
+
+                
+            </div>
+        </>
+    )
+}
