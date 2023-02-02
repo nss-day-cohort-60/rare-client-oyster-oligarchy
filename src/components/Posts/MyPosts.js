e23b3f1663971e0c10f8609a79543946724c6413
nathanpyles.91@gmail.com <nathanpyles.91@gmail.com>
2023-02-02 11:34:34 -0500

    CSS touchups

--- src/components/Posts/MyPosts.js
+++ src/components/Posts/MyPosts.js
@@ -3,12 +3,14 @@ import { useNavigate } from "react-router-dom"
 import "./Posts.css"
 
 
-const localUser = localStorage.getItem("auth_token")
-const userObject = JSON.parse(localUser)
+
 
 export const MyPosts = () => {
-    const [posts, setPosts] = useState ([])
+    const [posts, setPosts] = useState([])
     const navigate = useNavigate()
+
+    const localUser = localStorage.getItem("auth_token")
+    const userObject = JSON.parse(localUser)
 
     console.log(userObject)
 
@@ -16,14 +18,14 @@ export const MyPosts = () => {
         fetch(`http://localhost:8088/posts?user_id=${userObject}`)
             .then((res) => res.json())
             .then((postsArray) => {
-            setPosts(postsArray)
+                setPosts(postsArray)
             })
     }, [])
 
     const formatDate = (postObj) => {
         let formattedDate = postObj.publication_date.split("-")
-        formattedDate = [formattedDate[1],formattedDate[2],formattedDate[0]]
-        return(formattedDate.join("/"))
+        formattedDate = [formattedDate[1], formattedDate[2], formattedDate[0]]
+        return (formattedDate.join("/"))
     }
 
     // const deleteButton = (id) => {
@@ -37,36 +39,36 @@ export const MyPosts = () => {
     //     }} className="schedule_delete">Delete</button>
     // }
 
-        return(
-            <>
+    return (
+        <>
             <div className="top-of-posts">
                 <h1 className="posts-title">Posts</h1>
             </div>
-            
+
             <div className="posts-container">
                 {posts.map((postObj) => {
                     if (postObj.user_id === userObject)
                         return (
                             <div className="activity-card" key={postObj.id}>
-                            <div className="each-post">
-                                <h3 className="post-title">{postObj.title}</h3>
-                                <img src={postObj.image_url}/>
-                                <p className="post-details">{postObj.content}</p>
-                                <p className="post-details">Author: {postObj.first_name} {postObj.last_name}</p>
-                                <p className="post-details">Category: {postObj.label}</p>
-                                <p className="post-details">Posted on: {formatDate(postObj)}</p>
-                                <button>Edit</button>
-                                <button>Delete</button>
-                                {/* {deleteButton(activityObj.id)} */}
-                            </div>
+                                <div className="each-post">
+                                    <h3 className="post-title">{postObj.title}</h3>
+                                    <img src={postObj.image_url} />
+                                    <p className="post-details">{postObj.content}</p>
+                                    <p className="post-details">Author: {postObj.first_name} {postObj.last_name}</p>
+                                    <p className="post-details">Category: {postObj.label}</p>
+                                    <p className="post-details">Posted on: {formatDate(postObj)}</p>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                    {/* {deleteButton(activityObj.id)} */}
+                                </div>
                             </div>
                         )
                     else {
                         return <>
                         </>
                     }
-                    })}
-                </div>
-            </>
-        )
+                })}
+            </div>
+        </>
+    )
 }
